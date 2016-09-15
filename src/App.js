25e2afe5d0e40be09c0a4c005c4baf72fc9a400b
Sam Parsons <sjparsons@gmail.com>
2016-09-15 03:41:41 -0500

    Update the App styles, typography and include bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const { children } = this.props
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="app-header">
+          <div className="app-brand"><Link to="/" style={{color: 'inherit'}}>
+            <img src={logo} className="app-logo" alt="React logo" />
+            <h2 className="app-title">React Minneapolis</h2>
+          </Link></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="app-tag">
+          <div className="container">
+          The Meetup about the React JavaScript framework in the City of Lakes.
+          </div>
+        </div>
+        <nav className="app-nav  text-center">
+          <div className="container">
+            <Link to="/sponsorship">Sponsorship</Link>{' '}
+            <a target="_blank" href="//feedback.reactmpls.io">Feedback</a>
+          </div>
+        </nav>
+        <div className="app-content">
+          {children}
+        </div>
+
+
       </div>
     );
   }
